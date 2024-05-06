7da0279671e946a5f0365d86f46831446aa798d0
100495754 <158482282+100495754@users.noreply.github.com>
2024-05-06 18:25:38 +0200

    funciona todo pero falla en los join

--- ssoo_p3_multihilo_2024/store_manager.c
+++ ssoo_p3_multihilo_2024/store_manager.c
@@ -21,7 +21,7 @@ queue* elemsQueue;
 pthread_mutex_t mutex;
 pthread_cond_t no_lleno;
 pthread_cond_t no_vacio;
-int total = 0;//variable global para los profits (evita que los hilos den diferentes profits)
+int total = 0, operaciones = 0, retiradas = 0;//variable global para los profits (evita que los hilos den diferentes profits)
 
 
 /* Esta funcion recoge la primera linea del fichero entrante para guardar el numero maximo de operaciones a realizar*/
@@ -55,14 +55,15 @@ void almacenar(int fd){
     char units[4], buff[1];
     ssize_t nread;
     struct element e;
-    int salto = 0;
+    int salto = 0, temporal = 0;
 
     // Inicia un bucle infinito que se rompe solo si 'read' falla
     while((nread = read(fd, buff, 1)) > 0) {
+        if (temporal==0){e.product_id = 1;temporal++;}
+        else{e.product_id = atoi(buff);}
+        //e.product_id = atoi(buff);
 
-        e.product_id = atoi(buff);
-        printf("  almacenar1: %d   %d\n", atoi(buff), e.product_id);
-        read(fd, buff, 1);
+        read(fd, buff, 1);//BLANCO
         read(fd, buff, 1);
 
         if (buff[0] == 'P') {
@@ -92,130 +93,173 @@ void almacenar(int fd){
             }
         }
         int unidades = 0;
-        if (i==2){
+        if (i==3){
             unidades = units[0]*100 + units[1]*10 + units[2];
         }
-        else if (i==1){
+        else if (i==2){
             unidades = units[0]*10 + units[1];
         }
-        else if (i==0){
+        else if (i==1){
             unidades = units[0];
         }
         e.units = unidades;
         queue_put(elemsQueue, &e);
+
     }
 }
 
 
 /* Esta funcion coge datos de la cola que guarda los elementos del fichero y almacena de uno en uno en el buffer compartido*/
-void *producir(int operations){
-    //sem_wait(&sem_empty);
-
-    pthread_mutex_lock(&mutex);//bloqueo por entrar en seccion critica
-    //seccion critica
-    while (sharedQueue->capacity == sharedQueue->count){
-        pthread_cond_wait(&no_lleno, &mutex);
+void *producir(void* arg){
+    if (arg == NULL) {
+        printf("Productor recibió argumento NULL.\n");
+        return NULL;
     }
-    for(int i = 0; i < operations; i++ ){
-        struct element *newElem;
-        newElem = queue_get(elemsQueue);
-        queue_put(sharedQueue, newElem);
-        pthread_cond_signal(&no_vacio);// esto manda la señal de que el buffer ya no esta vacio
-    }
+    int id = *(int*)arg;
+    printf("Productor %d iniciado.\n", id);
+    while(operaciones < elemsQueue->capacity){
+        //sem_wait(&sem_empty);
+        pthread_mutex_lock(&mutex);//bloqueo por entrar en seccion critica
+        printf("Entro en seccion critica %d\n", id);
+        //seccion critica
+        while (sharedQueue->capacity == sharedQueue->count){
+            printf("Soy %d esperando a tener hueco para escribir\n ",id);
+            printf("\n");
+            pthread_cond_wait(&no_lleno, &mutex);
+            //printf("Soy %d dejo de esperar\n ",id);
+        }
+        if (operaciones<elemsQueue->capacity){
+            struct element *newElem = queue_get(elemsQueue);
+            if (newElem == NULL) {
+                printf("Failed to get new element.\n");
+                pthread_mutex_unlock(&mutex);
+                return NULL;
+            }
+            queue_put(sharedQueue, newElem);
+            operaciones++;
+            pthread_cond_signal(&no_vacio);// esto manda la señal de que el buffer ya no esta vacio
+            //salida de seccion critica
+            printf("Voy a salir de critica soy: %d \n ",id);
+            printf("Productor dice que quedan %d items por coger de elemsqueue \n", elemsQueue->count);
+            pthread_mutex_unlock(&mutex);
+            //sem_post(&sem_full);
+            printf("Ya fuera de critica soy: %d \n ",id);
+            printf("\n ");
+        }
+        else{
+            printf("PRODUCTOR %d TERMINA DEBIDO A QUE NO HAY MAS OPERACIONES\n", id);
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
 
-    //salida de seccion critica
-    pthread_mutex_unlock(&mutex);
-    //sem_post(&sem_full);
+    }
+    printf("Productor %d terminado \n", id);
     return NULL;
 }
 
-void *consumir(int product_stock [5]){
-    //sem_wait(&sem_empty);
+void *consumir(int product_stock [5], void* arg){
+    int id = *(int*)arg;
+    printf("Consumidor %d iniciado.\n", id);
+    while(retiradas < elemsQueue->capacity){
+        //sem_wait(&sem_full);
+        pthread_mutex_lock(&mutex);//bloqueo por entrar en seccion critica
+        printf("Consumidor %d entra en critica.\n", id);
+        //seccion critica
+        while (sharedQueue->count == 0){
+            printf("Soy el consumidor %d esperando a tener hueco para escribir\n ",id);
+            pthread_cond_wait(&no_vacio, &mutex);
+        }
+        struct element *getElement = queue_get(sharedQueue);
+        retiradas++;
+        if (getElement == NULL) {
+            printf( "Failed to get new element.\n");
+            printf("Consumidor %d terminado \n", id);
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
+        pthread_cond_signal(&no_lleno);// esto manda la señal de que el buffer ya no esta vacio
 
-    pthread_mutex_lock(&mutex);//bloqueo por entrar en seccion critica
-    //seccion critica
-    while (sharedQueue->capacity == 0){
-        pthread_cond_wait(&no_vacio, &mutex);
-    }
-    struct element *getElement;
-    getElement = queue_get(sharedQueue);
-    pthread_cond_signal(&no_lleno);// esto manda la señal de que el buffer ya no esta vacio
-    //salida de seccion critica
-    pthread_mutex_unlock(&mutex);
+        //operaciones
+        if (( getElement->product_id) == 1){
+            if(getElement->op == 1){//PURCHASE
+                int coste = 0;
+                coste = getElement->units * 2;
+                product_stock[0] = product_stock[0] + getElement->units;
+                total = total - coste;
+            }
+            else if (getElement->op == 2){//SALE
+                int beneficio = 0;
+                beneficio = getElement->units * 3;
+                product_stock[0] = product_stock[0] - getElement->units;
+                total = total + beneficio;
+            }
+        }
+        else if (( getElement->product_id) == 2){
+            if(getElement->op == 1){
+                int coste = 0;
+                coste = getElement->units * 3;
+                product_stock[1] = product_stock[1] + getElement->units;
+                total = total - coste;
+            }
+            else if (getElement->op == 2){
+                int beneficio = 0;
+                beneficio = getElement->units * 5;
+                product_stock[1] = product_stock[1] - getElement->units;
+                total = total + beneficio;
+            }
+        }
+        else if ((getElement->product_id) == 3){
+            if(getElement->op == 1){
+                int coste = 0;
+                coste = getElement->units * 15;
+                product_stock[2] = product_stock[2] + getElement->units;
+                total = total - coste;
+            }
+            else if (getElement->op == 2){
+                int beneficio = 0;
+                beneficio = getElement->units * 20;
+                product_stock[2] = product_stock[2] - getElement->units;
+                total = total + beneficio;
+            }
+        }
+        else if ((getElement->product_id) == 4){
+            if(getElement->op == 1){
+                int coste = 0;
+                coste = getElement->units * 25;
+                product_stock[3] = product_stock[3] + getElement->units;
+                total = total - coste;
+            }
+            else if (getElement->op == 2){
+                int beneficio = 0;
+                beneficio = getElement->units * 40;
+                product_stock[3] = product_stock[3] - getElement->units;
+                total = total + beneficio;
+            }
+        }
+        else if ((getElement->product_id) == 5){
+            if(getElement->op == 1){
+                int coste = 0;
+                coste = getElement->units * 100;
+                product_stock[4] = product_stock[4] + getElement->units;
+                total = total - coste;
+            }
+            else if (getElement->op == 2){
+                int beneficio = 0;
+                beneficio = getElement->units * 125;
+                product_stock[4] = product_stock[4] - getElement->units;
+                total = total + beneficio;
+            }
+        }
 
-    //operaciones
-    if (( getElement->product_id) == 1){
-        if(getElement->op == 1){//PURCHASE
-            int coste = 0;
-            coste = getElement->units * 2;
-            product_stock[0] = product_stock[0] + getElement->units;
-            total = total - coste;
-        }
-        else if (getElement->op == 2){//SALE
-            int beneficio = 0;
-            beneficio = getElement->units * 3;
-            product_stock[0] = product_stock[0] - getElement->units;
-            total = total + beneficio;
-        }
+        //salida de seccion critica
+        printf("Consumidor dice que quedan %d items por coger y retiradas totales: %d\n", sharedQueue->count, retiradas);
+        printf("Consumidor %d saliendo de seccion critica.\n", id);
+        pthread_mutex_unlock(&mutex);
+        printf("Consumidor %d fuera de critica \n", id);
+        printf("\n ");
+        //sem_post(&sem_empty);
     }
-    else if (( getElement->product_id) == 2){
-        if(getElement->op == 1){
-            int coste = 0;
-            coste = getElement->units * 3;
-            product_stock[1] = product_stock[1] + getElement->units;
-            total = total - coste;
-        }
-        else if (getElement->op == 2){
-            int beneficio = 0;
-            beneficio = getElement->units * 5;
-            product_stock[1] = product_stock[1] - getElement->units;
-            total = total + beneficio;
-        }
-    }
-    else if ((getElement->product_id) == 3){
-        if(getElement->op == 1){
-            int coste = 0;
-            coste = getElement->units * 15;
-            product_stock[2] = product_stock[2] + getElement->units;
-            total = total - coste;
-        }
-        else if (getElement->op == 2){
-            int beneficio = 0;
-            beneficio = getElement->units * 20;
-            product_stock[2] = product_stock[2] - getElement->units;
-            total = total + beneficio;
-        }
-    }
-    else if ((getElement->product_id) == 4){
-        if(getElement->op == 1){
-            int coste = 0;
-            coste = getElement->units * 25;
-            product_stock[3] = product_stock[3] + getElement->units;
-            total = total - coste;
-        }
-        else if (getElement->op == 2){
-            int beneficio = 0;
-            beneficio = getElement->units * 40;
-            product_stock[3] = product_stock[3] - getElement->units;
-            total = total + beneficio;
-        }
-    }
-    else if ((getElement->product_id) == 5){
-        if(getElement->op == 1){
-            int coste = 0;
-            coste = getElement->units * 100;
-            product_stock[4] = product_stock[4] + getElement->units;
-            total = total - coste;
-        }
-        else if (getElement->op == 2){
-            int beneficio = 0;
-            beneficio = getElement->units * 125;
-            product_stock[4] = product_stock[4] - getElement->units;
-            total = total + beneficio;
-        }
-    }
-
-    //sem_post(&sem_full);
+    printf("Consumidor %d terminado \n", id);
     return NULL;
 }
 
@@ -251,50 +295,65 @@ int main (int argc, const char * argv[])
     elemsQueue = queue_init(max);
     printf("  capacity: %d\n", elemsQueue->capacity);
     almacenar(fd);
+    printf("%d", elemsQueue->count);
 
     //INICIALIZAMOS LAS VARIABLES DE LOS HILOS Y SUS VALORES
     int n_productores = atoi(argv[2]), n_consumidores = atoi(argv[3]), tam_buffer = atoi(argv[4]);
-    printf("  mAX: %d\n", n_productores);
     //inicializamos los hilos
     pthread_t productores[n_productores], consumidores[n_consumidores];
 
     //INICIALIZAMOS SEMAFOROS, MUTEX, ETC.
     pthread_cond_init(&no_lleno, NULL);
     pthread_cond_init(&no_vacio, NULL);
-    //sem_init(&sem_full, 0, 0);
     //sem_init(&sem_empty, 0, tam_buffer);
+    //sem_init(&sem_full, 0, 0);
     pthread_mutex_init(&mutex, NULL);
-    printf("  mAX: %d\n", n_productores);
     //inicio de la cola compartida
     sharedQueue = queue_init(tam_buffer);
-    printf("  mAX: %d\n", n_productores);
 
 
-    int reparto = max / n_productores;// repartimos las operaciones a los hilos productores
+    //int reparto = max / n_productores;// repartimos las operaciones a los hilos productores
 
     // creamos todos los hilos productores
     for (int i = 0; i < n_productores; i++){
-        if (pthread_create(&productores[i], NULL, producir(reparto), NULL) != 0) {
-            perror("Error al crear el hilo");
+        int* id_p = malloc(sizeof(int));
+        *id_p = i;
+        printf("Creando hilo prod %d \n", i);
+        if (pthread_create(&productores[i], NULL, producir, id_p) != 0) {
+            printf("Error al crear el hilo");
+            free(id_p); // Liberar memoria en caso de falla
             return 1;
         }
+        free(id_p);
     }
 
     //creamos los hilos consumidores
-    for (int i = 0; i < n_productores; i++){
-        if (pthread_create(&consumidores[i], NULL, consumir(product_stock), NULL) != 0) {
-            perror("Error al crear el hilo");
+    for (int i = 0; i < n_consumidores; i++){
+        int* id = malloc(sizeof(int));
+        *id = i;
+        printf("Creando hilo consumidor %d \n", i);
+        if (pthread_create(&consumidores[i], NULL, consumir(product_stock, id), NULL) != 0) {
+            printf("Error al crear el hilo");
+            free(id); // Liberar memoria en caso de falla
             return 1;
         }
     }
 
     // Esperamos a que todos los hilos productores terminen
     for (int i = 0; i < n_productores; i++) {
+        printf("Productor %d volvio a casa por navidad\n", i);
+        printf("\n");
         pthread_join(productores[i], NULL);
+        printf("Productor %d ha terminado y se ha unido correctamente.\n", i);
+        printf("\n");
     }
     // Esperamos a que todos los hilos consumidores terminen
     for (int i = 0; i < n_consumidores; i++) {
+        printf("Consumidor %d volvio a casa por navidad\n", i);
+        printf("\n");
         pthread_join(consumidores[i], NULL);
+        printf("Consumidor %d ha terminado y se ha unido correctamente.\n", i);
+        printf("\n");
     }
 
     //Limpiamos
@@ -305,6 +364,7 @@ int main (int argc, const char * argv[])
     //sem_destroy(&sem_full);
     //sem_destroy(&sem_empty);
     pthread_mutex_destroy(&mutex);
+
 
 
     profits = total;
